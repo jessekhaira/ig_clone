d344e6f2bbfaea899f9749df97c4f17822db983c
13jk59 <jesse.khaira10@gmail.com>
2020-11-18 19:04:58 -0700

    init commits, setting up the frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import '../stylesheets/App.css';
+import React from 'react';
+
+class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
+        init
+      </div>
+    )
+  }
+}
+
+export default App;
