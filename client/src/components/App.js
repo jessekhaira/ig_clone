72fcf1d5696236bcb93adc27b09a26beb0b14ae8
13jk59 <jesse.khaira10@gmail.com>
2020-11-19 17:35:42 -0700

    configuring and setting up frontend and backend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,16 @@ import React from 'react';
 import {createStore} from 'redux';
 
 class App extends React.Component{
+
+  async _toyReq() {
+    const data = await fetch('/toy');
+    document.getElementById('toy').innerHTML = data; 
+  }
+  
   render() {
     return (
       <div className="App">
-        init
+        <button id = 'toy' onClick = {this._toyReq}>init</button>
       </div>
     )
   }
