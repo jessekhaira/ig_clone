27646577a2d9d1bc64b8849c69d6ddc31fc3ad3c
13jk59 <jesse.khaira10@gmail.com>
2020-11-20 19:38:28 -0700

    styles almost done for signup page and register page

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -9,19 +9,23 @@ class Register extends React.Component {
     render() {
         return(
             <div id = "RegisterDiv" className = "auth_holder">
-                <h1 className = "instagram_name">Instagram Clone</h1>
-                <h2>Sign up to join in on the fun!</h2>
+                <div className = "auth_info">
+                
+                    <div className = "name_form_auth">
+                        <h1 className = "instagram_name">Instagram Clone</h1>
+                        <h2>Sign up to join in on the fun!</h2>
+                        <form id = "RegisterForm" className = "form_auth">
+                            <input type = "email" placeholder = "Email"></input>
+                            <input type = "text" placeholder = "Full Name"></input>
+                            <input type = "text" placeholder = "Username"></input>
+                            <input type = "password" placeholder = "Password"></input>
+                            <input type = "submit" placeholder = "Sign Up"></input>
+                        </form>
+                    </div>
 
-                <form id = "RegisterForm" className = "form_auth">
-                    <input type = "email" placeholder = "Email"></input>
-                    <input type = "text" placeholder = "Full Name"></input>
-                    <input type = "text" placeholder = "Username"></input>
-                    <input type = "password" placeholder = "Password"></input>
-                    <input type = "submit" placeholder = "Sign Up"></input>
-                </form>
-
-                <div id = "redirect_signIn">
-                    <p>Have an account? <Link to = "/">Log in</Link></p>
+                    <div className = "auth_link">
+                        <p>Have an account? <Link to = "/">Log in</Link></p>
+                    </div>
                 </div>
             </div>
 
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -10,15 +10,18 @@ class SignIn extends React.Component {
     render() {
         return(
             <div id = "register" className = "auth_holder">
-                <h1 className = "instagram_name">Instagram Clone</h1>
-
-                <form id = "sign_in" className = "form_auth">
-                    <input type = "email" placeholder = "Email or username"></input>
-                    <input type = "password" placeholder = "Password"></input>
-                    <input type = "submit" placeholder = "Log In"></input>
-                </form>
-                <div>
-                    <p>Don't have an account?<Link to = "/register"> Sign up</Link></p>
+                <div className = "auth_info">
+                    <div className = "name_form_auth">
+                        <h1 className = "instagram_name">Instagram Clone</h1>
+                        <form id = "sign_in" className = "form_auth">
+                            <input type = "email" placeholder = "Email or username"></input>
+                            <input type = "password" placeholder = "Password"></input>
+                            <input type = "submit" placeholder = "Log In"></input>
+                        </form>
+                    </div>
+                    <div className = "auth_link">
+                        <p>Don't have an account?<Link to = "/register"> Sign up</Link></p>
+                    </div>
                 </div>
             </div>
         )
