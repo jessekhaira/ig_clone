4f200ce52841807f9b8baaceb5631857daa5d4c9
13jk59 <jesse.khaira10@gmail.com>
2020-11-20 19:50:14 -0700

    added documentation

--- client/src/components/AppWrapper.js
+++ client/src/components/AppWrapper.js
@@ -2,6 +2,10 @@ import App from './App';
 import {Provider} from 'react-redux';
 import {reduxStore} from '../redux/createStore';
 
+/**
+ * This function wraps the React app with Provider from react redux in order to provide
+ * the redux store to the application. 
+ */
 function AppWrapper() {
     return(
         <Provider store = {reduxStore}>
