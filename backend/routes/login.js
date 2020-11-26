a55613762c80e6d9f9253f47cc4bfa852a1777b6
13jk59 <jesse.khaira10@gmail.com>
2020-11-25 18:51:47 -0700

    added documentation backend

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var validator = require('express-validator');
 
 
+/**
+ * This API endpoint mounted on the router responds to GET request to the '/login' route. It has the responsibility of sanitizing
+ * the input arguments recieved, and then validating if they belong to a user in the database. If they do, the client will be notified
+ * that verification was succesful and to redirect to the appropriate view. 
+ */
 router.post('/', [
     validator.body('username').escape(),
     validator.body('password').escape(),
