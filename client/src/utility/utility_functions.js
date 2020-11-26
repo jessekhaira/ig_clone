a7dd803870144652525ded83a97e801c57115f86
13jk59 <jesse.khaira10@gmail.com>
2020-11-25 18:55:31 -0700

    added documentation

--- client/src/utility/utility_functions.js
+++ client/src/utility/utility_functions.js
@@ -1,3 +1,12 @@
+/**
+ * This function is a utility function used to set the display of a variable number of input DOM elements. 
+ * Useful in multiple places in the frontend as displays of elements are changed regularly. 
+ * 
+ * Expects the displays array to match the length of the args array, where displays[i] is a String describing the
+ * display of args[i]. 
+ * @param {String[]} displays Array of strings containing the CSS displays for a wide variety of elements
+ * @param  {HTMLElement[]} args Array of DOM elements 
+ */
 export function setDisplay(displays, ...args) {
     for (let [i,arg] of args.entries()) {
         const display = displays[i]; 
