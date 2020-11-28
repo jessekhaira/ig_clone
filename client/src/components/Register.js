d536acba2f59a363f46dd9aac62bdf647523aed8
13jk59 <jesse.khaira10@gmail.com>
2020-11-27 20:16:04 -0700

    finished validation of inputs by the client

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -16,6 +16,7 @@ class Register extends React.Component {
 
     componentDidMount() {
         document.getElementById('signup_button').disabled = true; 
+        document.getElementsByClassName('validation_error')[0].style.display = 'none'; 
     }
 
     _register(e) {
@@ -46,18 +47,56 @@ class Register extends React.Component {
         const username_inp = document.getElementById('username_input').value;
         const pw_inp = document.getElementById('pw_input').value;
         const date_of_birth_inp = document.getElementById('date_of_birth_input').value;
-
+        const error_display = document.getElementsByClassName('validation_error')[0];
+        // every time we send a new request, we want the error message displayed (if any) to be reset
+        error_display.style.display = 'none'; 
         // we can ensure the email and username follow the correct format in the client, but in terms of making sure 
         // this email isn't already registered to a user, and the username isn't already taken, we'll have to send 
         // the POST to theserver and check 
         const is_email_valid = this._validateEmail(email_inp);
-
         const is_username_valid = this._validateUsername(username_inp); 
         const is_date_of_birth_valid = this._validateDateOfBirth(date_of_birth_inp); 
         if (is_email_valid && is_username_valid && is_date_of_birth_valid) {
             this._attemptToCreateUser(email_inp, name_inp, username_inp, pw_inp, date_of_birth_inp);
         }
+        // if there are multiple errors with the validation, just address the first one and display error for that
+        else if (!is_email_valid) {
+            error_display.innerHTML = 'Please enter a valid email address'; 
+            error_display.style.display = 'block';
+        }
+
+        else if (!is_username_valid) {
+            error_display.innerHTML = 'Usernames can only use letters, numbers, underscores and periods';
+            error_display.style.display = 'block';
+        }
+
+        else if (!is_date_of_birth_valid) {
+            error_display.innerHTML = 'Please enter a valid date of birth';
+            error_display.style.display = 'block';
+        }
     }
+
+    async _attemptToCreateUser(email, full_name, username_inp, pw_inp, date_of_birth) {
+        // try to create the user if there are no issues in the backend
+        try {
+            let register_result = fetch('/register', {
+                method: "POST",
+                body: JSON.stringify({
+                    email,
+                    full_name, 
+                    username_inp,
+                    pw_inp,
+                    date_of_birth
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8'
+                }
+            }); 
+        }
+        catch (err) {
+            console.log(err); 
+        }
+    } 
 
 
     _validateEmail(email_inp) {
@@ -82,27 +121,6 @@ class Register extends React.Component {
         return true; 
     }
 
-    async _attemptToCreateUser(email, full_name, username_inp, pw_inp, date_of_birth) {
-        // try to create the user if there are no issues in the backend
-        try {
-            let register_result = fetch('/register', {
-                method: "POST",
-                body: JSON.stringify({
-                    email,
-                    full_name, 
-                    username_inp,
-                    pw_inp,
-                    date_of_birth
-                }),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8'
-                }
-            }); 
-        }
-        catch (err) {
-            console.log(err); 
-        }
-    } 
 
     _focus_text_input_to_date (e){
         e.currentTarget.type = "date";
@@ -136,8 +154,11 @@ class Register extends React.Component {
                                     <div className="sk-chase-dot"></div>
                                     <div className="sk-chase-dot"></div>
                                 </div>
-                            </button>                       
+                            </button>
                         </form>
+                        <div className = "validation_error_div">
+                            <p className = "validation_error"></p>                       
+                        </div>
                     </div>
 
                     <div className = "auth_link">
