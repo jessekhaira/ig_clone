0dbf5e14836cd49c51ddf990370cc3921124584d
13jk59 <jesse.khaira10@gmail.com>
2020-12-05 20:19:18 -0700

    connected the newly created slice of state to the redux store and added the state + action creators in the react redux maps

--- client/src/redux/createStore.js
+++ client/src/redux/createStore.js
@@ -3,12 +3,15 @@ import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
 import {logger} from 'redux-logger';
 import thunk from 'redux-thunk';
 import {currentUserReducer} from './features/currentUserSlice';
+import {statusReducer} from './features/statusSlice'
+
 const loggerMiddleware = createLogger();
 
 // overall 
 export const reduxStore = configureStore({
     reducer: {
-        current_user: currentUserReducer
+        current_user: currentUserReducer,
+        status: statusReducer
     },
     middleware: [logger, thunk]
 });
--- client/src/redux/reactReduxMaps.js
+++ client/src/redux/reactReduxMaps.js
@@ -1,15 +1,21 @@
 import {set_current_user, remove_current_user} from './features/currentUserSlice';
+import {set_status_failed, set_status_idle, set_status_loading, set_status_succeeded} from './features/statusSlice';
 
 function mapStateToProps(state) {
     return {
-        current_user: state.current_user.current_user
+        current_user: state.current_user.current_user,
+        status: state.status
     }
 }
 
 function mapDispatchToProps(dispatch) {
     return {
         set_current_user: (current_user) => dispatch(set_current_user(current_user)),
-        remove_current_user: () => dispatch(remove_current_user())
+        remove_current_user: () => dispatch(remove_current_user()),
+        set_status_failed: () => dispatch(set_status_failed()),
+        set_status_idle: () => dispatch(set_status_idle()),
+        set_status_loading: () => dispatch(set_status_loading()),
+        set_status_succeeded: () => dispatch(set_status_succeeded())
     }
 }
 
