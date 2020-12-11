c610874ce071b23972a98de847e8487f0bbb735f
13jk59 <jesse.khaira10@gmail.com>
2020-12-10 20:21:57 -0700

    worked on search bar styling + javascript interactivity inside the NavBar component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,6 +5,50 @@ class NavBar extends React.Component{
     constructor(props) {
         super(props); 
     }
+    
+    componentDidMount() {
+
+    }
+
+    _searchBarFocus(e) {
+        e.preventDefault();  
+        console.log(e);   
+        const search_bar = document.getElementById('search_bar');
+        const icon_input_div = document.getElementById('icon_input_div');
+        document.getElementById('delete_inp_text_icon').style.display = 'block'; 
+        // hide the div containing text, show the input tag
+        const inp_tag = document.getElementById('search_input');
+        inp_tag.style.display = 'block';
+        inp_tag.focus(); 
+        const inp_display_text = document.getElementById('inp_display_text');
+        inp_display_text.style.display = 'none'; 
+    }
+
+    _searchBarBlur(e) {
+        e.preventDefault(); 
+        console.log(e); 
+        const search_bar = document.getElementById('search_bar');
+        const icon_input_div = document.getElementById('icon_input_div');
+        search_bar.style.justifyContent = 'center';
+        icon_input_div.style.justifyContent = 'center';
+        document.getElementById('delete_inp_text_icon').style.display = 'none'; 
+        const inp_tag = document.getElementById('search_input');
+        inp_tag.style.display = 'none';
+        const inp_display_text = document.getElementById('inp_display_text');
+        inp_display_text.style.display = 'block'; 
+        inp_display_text.innerHTML = (inp_tag.value ? inp_tag.value: "Search"); 
+    }
+
+    _searchDelete(e) {
+        e.preventDefault(); 
+        console.log(e); 
+        const search_bar = document.getElementById('search_bar');
+        search_bar.blur(); 
+        const inp_tag = document.getElementById('search_input');
+        inp_tag.value = ''; 
+    }
+
+
 
     render() {
         return(
@@ -13,11 +57,13 @@ class NavBar extends React.Component{
                     <h1 className = "instagram_name">Instagram Clone</h1>
                 </div>
 
-                <div id = "search_bar">
-                    <div id = "search_icon_div">
+                <div id = "search_bar" onClick = {this._searchBarFocus} onBlur = {this._searchBarBlur}>
+                    <div id = "icon_input_div">
                         <i class="fas fa-search search_icon"></i>
+                        <div id = "inp_display_text">Search</div>
+                        <input id = "search_input" type = "text" placeholder = "Search"></input>
+                        <i id = "delete_inp_text_icon" class="fas fa-times-circle position_icon" onClick = {this._searchDelete}></i>
                     </div>
-                    <input id = "search_input" type = "text" placeholder = "Search"></input>
                 </div>
 
                 <div id = "navbar_options">
