aae9930d82bae1f28b7acbaca17d782b7e17c899
13jk59 <jesse.khaira10@gmail.com>
2020-12-13 15:02:32 -0700

    refactored code for profile icon settings into functional presentational component

--- client/src/components/LoggedInViews/NavBar.js
+++ client/src/components/LoggedInViews/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import {Link} from "react-router-dom";
 import {withRouter} from 'react-router-dom';
+import {ProfileIconSettings} from './ProfileIconSettings';
 
 /**
  * This class represents a React Component that represents the navigation
@@ -10,12 +11,11 @@ import {withRouter} from 'react-router-dom';
 class NavBar extends React.Component{
     constructor(props) {
         super(props); 
-
-        this._logout = this._logout.bind(this); 
         this._documentClickListener = this._documentClickListener.bind(this);
     }
 
     componentDidMount() {
+        document.getElementById('profile_settings').style.display = 'none'; 
         document.addEventListener('click', this._documentClickListener);
     }
 
@@ -66,16 +66,6 @@ class NavBar extends React.Component{
         inp_tag.value = ''; 
     }
 
-    _logout(e) {
-        this.props.remove_curr_user(); 
-    }
-
-    _showProfileSettings(e) {
-        e.preventDefault();
-        const profile_settings = document.getElementById('profile_settings');
-        profile_settings.style.display = (profile_settings.style.display === 'block' ? 'none':'block');
-    }
-
     render() {
         return(
             <div id = "navbar_container">
@@ -109,55 +99,9 @@ class NavBar extends React.Component{
                         </Link>
 
                         <i id = "notifications" class="far fa-heart navbar_icons margin_class" ></i>
-                        <div id = "profile_icon" className = "margin_class" onClick = {this._showProfileSettings}>
-                            <img id = "profile_img" className = "options_imgs" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
-                            <div id = "profile_settings">
-                                <Link to = {`/${this.props.current_user}`}>
-                                    <div id = "go_to_profile_div" className = "profile_settings_options">
-                                        <div className = "profile_settings_icons">
-                                            <img className = "icons_settings" src = "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png"></img>
-                                        </div>
-                                        <div className = "profile_settings_descr">
-                                            <p>Profile</p>
-                                        </div>
-                                    </div>
-                                </Link>
-
-                                <div id = "saved_div" className = "profile_settings_options">
-                                    <div className = "profile_settings_icons">
-                                        <img className = "icons_settings" src = "https://cdn2.iconfinder.com/data/icons/electronic-line-3/64/Bookmark_favourite_pin_saved_interface_icon-512.png"></img>
-                                    </div>
-                                    <div className = "profile_settings_descr">
-                                        <p>Saved</p>
-                                    </div>
-                                </div>
-
-                                <div id = "settings_div" className = "profile_settings_options">
-                                    <div className = "profile_settings_icons">
-                                        <img className = "icons_settings" src = "https://image.flaticon.com/icons/png/512/126/126472.png"></img>
-                                    </div>
-                                    <div className = "profile_settings_descr">
-                                        <p>Settings</p>
-                                    </div>
-                                </div>
-
-                                <div id = "switch_accounts_div" className = "profile_settings_options">
-                                    <div className = "profile_settings_icons">
-                                        <img className = "icons_settings" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Refresh_icon.svg/1200px-Refresh_icon.svg.png"></img>
-                                    </div>
-                                    <div className = "profile_settings_descr">
-                                        <p>Switch Accounts</p>
-                                    </div>
-                                </div>
-
-                                <hr id = "hr_settings"></hr>
-                                <div id = "log_out_div" className = "profile_settings_options" onClick = {this._logout}>
-                                    <div className = "profile_settings_descr logout">
-                                        <p>Log Out</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <ProfileIconSettings 
+                            remove_curr_user = {this.props.remove_curr_user}
+                            current_user = {this.props.current_user} />
                     </div>
                 </nav>
             </div>
---
+++ client/src/components/LoggedInViews/ProfileIconSettings.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+function ProfileIconSettings(props) {
+    function _logout() {
+        props.remove_curr_user(); 
+    }
+
+    function _showProfileSettings(e) {
+        e.preventDefault();
+        const profile_settings = document.getElementById('profile_settings');
+        profile_settings.style.display = (profile_settings.style.display === 'block' ? 'none':'block');
+    }
+    
+    return(
+        <div id = "profile_icon" className = "margin_class" onClick = {_showProfileSettings}>
+            <img id = "profile_img" className = "options_imgs" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            <div id = "profile_settings">
+                <Link to = {`/${props.current_user}`}>
+                    <div id = "go_to_profile_div" className = "profile_settings_options">
+                        <div className = "profile_settings_icons">
+                            <img className = "icons_settings" src = "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png"></img>
+                        </div>
+                        <div className = "profile_settings_descr">
+                            <p>Profile</p>
+                        </div>
+                    </div>
+                </Link>
+
+                <div id = "saved_div" className = "profile_settings_options">
+                    <div className = "profile_settings_icons">
+                        <img className = "icons_settings" src = "https://cdn2.iconfinder.com/data/icons/electronic-line-3/64/Bookmark_favourite_pin_saved_interface_icon-512.png"></img>
+                    </div>
+                    <div className = "profile_settings_descr">
+                        <p>Saved</p>
+                    </div>
+                </div>
+
+                <div id = "settings_div" className = "profile_settings_options">
+                    <div className = "profile_settings_icons">
+                        <img className = "icons_settings" src = "https://image.flaticon.com/icons/png/512/126/126472.png"></img>
+                    </div>
+                    <div className = "profile_settings_descr">
+                        <p>Settings</p>
+                    </div>
+                </div>
+
+                <div id = "switch_accounts_div" className = "profile_settings_options">
+                    <div className = "profile_settings_icons">
+                        <img className = "icons_settings" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Refresh_icon.svg/1200px-Refresh_icon.svg.png"></img>
+                    </div>
+                    <div className = "profile_settings_descr">
+                        <p>Switch Accounts</p>
+                    </div>
+                </div>
+
+                <hr id = "hr_settings"></hr>
+                <div id = "log_out_div" className = "profile_settings_options" onClick = {_logout}>
+                    <div className = "profile_settings_descr logout">
+                        <p>Log Out</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export {ProfileIconSettings}; 
