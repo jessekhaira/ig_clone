f57c92c82e4f410c7c544e7f7cf242fa594aff13
13jk59 <jesse.khaira10@gmail.com>
2020-12-15 19:54:36 -0700

    worked on notifications dropdown

--- client/src/components/LoggedInViews/Notifications.js
+++ client/src/components/LoggedInViews/Notifications.js
@@ -10,8 +10,39 @@ function Notifications(props) {
             <div className = "top_triangle notif_triangle"></div>
             <div id ="notifications_holder">
                 <div id = "follow_requests">
+                    <div id = "follow_req_info_container">
+                        <div id = "num_follow_req">1</div>
+                        <div id = "follow_req_info">
+                            <h3>Follow Requests</h3>
+                            <p>Approve or ignore requests</p>
+                        </div>
+                    </div>
+                    <div className = "side_triangle"></div>
                 </div>
-                <div id = "notifications">
+                <div id = "notificationDiv">
+                    <div className = "notification">
+
+                    </div>
+                    <hr className = "notification_hr"></hr>
+                    <div className = "notification">
+
+                    </div>
+                    <hr className = "notification_hr"></hr>
+                    <div className = "notification">
+
+                    </div>
+                    <hr className = "notification_hr"></hr>
+                    <div className = "notification">
+                        
+                    </div>
+                    <hr className = "notification_hr"></hr>
+                    <div className = "notification">
+
+                    </div>
+                    <hr className = "notification_hr"></hr>
+                    <div className = "notification">
+                        
+                    </div>
                 </div>
             </div>
         </div>
