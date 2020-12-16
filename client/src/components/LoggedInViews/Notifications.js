21d1ba24fdaedc7e86d75c08bb47fc931c288e9d
13jk59 <jesse.khaira10@gmail.com>
2020-12-15 21:03:00 -0700

    finished template for the notifications css + html, have to write up programmatic way to add notifications in

--- client/src/components/LoggedInViews/Notifications.js
+++ client/src/components/LoggedInViews/Notifications.js
@@ -21,7 +21,23 @@ function Notifications(props) {
                 </div>
                 <div id = "notificationDiv">
                     <div className = "notification">
-
+                        <div className = "userInformation_notif">
+                            <div className = "notif_profile_img_div ">
+                                <img className = "notif_profile_img" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+                            </div>
+                            <div className = "notif_username_div">
+                                <p className = "notif_username">1231232131231231213123123121</p>
+                            </div>
+                            <div className = "notif_descr_div">
+                                <p className = "notif_descr">liked your photo</p>
+                            </div> 
+                            <div className = "notif_time_elapsed_div">
+                                <p className = "notif_time_elapsed">2d</p>
+                            </div>
+                        </div>
+                        <div className = "photo_notif_targets_div">
+                            <img className = "photo_notif_targets" src = "https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+                        </div>
                     </div>
                     <hr className = "notification_hr"></hr>
                     <div className = "notification">
@@ -47,6 +63,10 @@ function Notifications(props) {
             </div>
         </div>
     )
+}
+
+function createNotificationDivs() {
+
 }
 
 
