e46b7dfadfe0084f916746ad72ebc47563c6b8ef
13jk59 <jesse.khaira10@gmail.com>
2020-12-21 13:53:49 -0700

    updated user profile component to have local state and have spinner divs along with a div for the case that the user isn't found

--- client/src/components/LoggedInViews/UserProfile/UserProfile.js
+++ client/src/components/LoggedInViews/UserProfile/UserProfile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {UserProfileInformation} from './UserProfileInformation';
 import {UserProfileToggleViews} from './UserProfileToggleViews';
 import {UserProfilePosts} from './UserProfilePosts';
@@ -7,8 +7,44 @@ import jwt_decode from 'jwt-decode';
 
 
 function UserProfile () {
+    // local state values needed in order to conditionally render either a spinner 
+    // or a message indicating the user doesn't exist in our database 
+    const [currentlyFetching, setCurrentlyFetching] = useState(false); 
+    const [userNotFound, setUserNotFound] = useState(false); 
+
+    async function aysncCallToMountInformation(endpoint) {
+        try {
+            await checkTokenExpirationMiddleware(); 
+            const username = jwt_decode(localStorage.getItem('accessToken')).username; 
+            const returned_profile_info_raw = await fetch(`/${username}/${endpoint}`, {
+                headers: {
+                    authorization: localStorage.getItem('accessToken')
+                }
+            }); 
+    
+            const returned_profile_info_json= await returned_profile_info_raw.json(); 
+            return returned_profile_info_json; 
+        }
+        catch(err) {
+            console.log(err); 
+            _authenticationErrorLogOut();
+        }
+    }
     return (
         <div id = "user_profile_top_level_div">
+            {currentlyFetching ? 
+                <div id = "spinner_div_notifications" className="sk-chase sk-chase-userprofile">
+                    <div className="sk-chase-dot sk-chase-dot-notif"></div>
+                    <div className="sk-chase-dot sk-chase-dot-notif"></div>
+                    <div className="sk-chase-dot sk-chase-dot-notif"></div>
+                    <div className="sk-chase-dot sk-chase-dot-notif"></div>
+                    <div className="sk-chase-dot sk-chase-dot-notif"></div>
+                    <div className="sk-chase-dot sk-chase-dot-notif"></div>
+                </div>
+            : userNotFound ? 
+                <div id = "user_not_found_container">
+                </div>
+            :
             <div id = "resize_together_container">
                 <UserProfileInformation 
                     aysncCallToMountInformation = {aysncCallToMountInformation}
@@ -18,28 +54,10 @@ function UserProfile () {
                     aysncCallToMountInformation = {aysncCallToMountInformation}
                 /> 
             </div>
+            }
         </div>
     )
 }
 
-
-async function aysncCallToMountInformation(endpoint) {
-    try {
-        await checkTokenExpirationMiddleware(); 
-        const username = jwt_decode(localStorage.getItem('accessToken')).username; 
-        const returned_profile_info_raw = await fetch(`/${username}/${endpoint}`, {
-            headers: {
-                authorization: localStorage.getItem('accessToken')
-            }
-        }); 
-
-        const returned_profile_info_json= await returned_profile_info_raw.json(); 
-        return returned_profile_info_json; 
-    }
-    catch(err) {
-        console.log(err); 
-        _authenticationErrorLogOut();
-    }
-}
 
 export {UserProfile}; 
