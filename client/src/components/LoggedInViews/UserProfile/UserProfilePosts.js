2a695877c0635d9f112fa7eda943a3ad2b91ef12
13jk59 <jesse.khaira10@gmail.com>
2020-12-20 19:22:06 -0700

    added hover divs for profile pictures to provide information on the pictures

--- client/src/components/LoggedInViews/UserProfile/UserProfilePosts.js
+++ client/src/components/LoggedInViews/UserProfile/UserProfilePosts.js
@@ -3,18 +3,51 @@ import React from 'react';
 function UserProfilePosts () {
     return (
         <div id = "user_profile_posts_overallholder">
-            <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
-            <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
-            <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            <div id = "grid_photo_div">
+                <div className = "grid_photo_information"></div>
+                <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            </div>
 
-            <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
-            <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
-            <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            <div id = "grid_photo_div">
+                <div className = "grid_photo_information"></div>
+                <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            </div>
 
-            <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
-            <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
-            <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            <div id = "grid_photo_div">
+                <div className = "grid_photo_information"></div>
+                <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            </div>
 
+            <div id = "grid_photo_div">
+                <div className = "grid_photo_information"></div>
+                <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            </div>
+            
+            <div id = "grid_photo_div">
+                <div className = "grid_photo_information"></div>
+                <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            </div>
+
+            <div id = "grid_photo_div">
+                <div className = "grid_photo_information"></div>
+                <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            </div>
+
+            <div id = "grid_photo_div">
+                <div className = "grid_photo_information"></div>
+                <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            </div>
+
+            <div id = "grid_photo_div">
+                <div className = "grid_photo_information"></div>
+                <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            </div>
+
+            <div id = "grid_photo_div">
+                <div className = "grid_photo_information"></div>
+                <img className = "grid_photo" src = "https://icon-library.com/images/generic-profile-icon/generic-profile-icon-23.jpg"></img>
+            </div>
+            
         </div>
     )
 }
