2ef68ba293cf38d919657bd082d4bb207f0be0a3
13jk59 <jesse.khaira10@gmail.com>
2021-01-05 14:35:41 -0700

    finished placeholders for stories portion on page

--- client/src/components/LoggedInViews/HomePage/HomePageStories.js
+++ client/src/components/LoggedInViews/HomePage/HomePageStories.js
@@ -3,11 +3,60 @@ import React from 'react';
 
 /**
  * This function represents a React component that renders the section of the homepage 
- * responsible for loading in user stories 
+ * responsible for loading in user stories. This wasn't part of the main scope of the project,
+ * so placeholder elements are present here. 
  */
 function HomePageStories () {
     return(
         <div id = 'home_page_stories_top_holder'>
+            <div className = 'home_page_story'>
+                <div className = 'img_wrapper_element'>
+                    <img className = 'story_profile_pic' src = 'https://www.newotani.co.jp/fileadmin/res/en/tokyo/offers/rosegarden/kv_01.jpg'></img>
+                </div>
+                <p className = 'story_username'>Placeholder1</p>
+            </div>
+            
+            <div className = 'home_page_story'>
+                <div className = 'img_wrapper_element'>
+                    <img className = 'story_profile_pic' src = 'https://www.newotani.co.jp/fileadmin/res/en/tokyo/offers/rosegarden/kv_01.jpg'></img>
+                </div>
+                <p className = 'story_username'>Placeholder2</p>
+            </div>
+
+            <div className = 'home_page_story'>
+                <div className = 'img_wrapper_element'>
+                    <img className = 'story_profile_pic' src = 'https://www.newotani.co.jp/fileadmin/res/en/tokyo/offers/rosegarden/kv_01.jpg'></img>
+                </div>
+                <p className = 'story_username'>Placeholder3</p>
+            </div>
+
+            <div className = 'home_page_story'>
+                <div className = 'img_wrapper_element'>
+                    <img className = 'story_profile_pic' src = 'https://www.newotani.co.jp/fileadmin/res/en/tokyo/offers/rosegarden/kv_01.jpg'></img>
+                </div>
+                <p className = 'story_username'>Placeholder4</p>
+            </div>
+
+            <div className = 'home_page_story'>
+                <div className = 'img_wrapper_element'>
+                    <img className = 'story_profile_pic' src = 'https://www.newotani.co.jp/fileadmin/res/en/tokyo/offers/rosegarden/kv_01.jpg'></img>
+                </div>
+                <p className = 'story_username'>Placeholder5</p>
+            </div>
+
+            <div className = 'home_page_story'>
+                <div className = 'img_wrapper_element'>
+                    <img className = 'story_profile_pic' src = 'https://www.newotani.co.jp/fileadmin/res/en/tokyo/offers/rosegarden/kv_01.jpg'></img>
+                </div>
+                <p className = 'story_username'>Placeholder6</p>
+            </div>
+
+            <div className = 'home_page_story'>
+                <div className = 'img_wrapper_element'>
+                    <img className = 'story_profile_pic' src = 'https://www.newotani.co.jp/fileadmin/res/en/tokyo/offers/rosegarden/kv_01.jpg'></img>
+                </div>
+                <p className = 'story_username'>Placeholder7</p>
+            </div>
         </div>
     )
 }
