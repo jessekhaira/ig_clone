79d91b5cd85129aa72432bf6e177a83a816c73af
13jk59 <jesse.khaira10@gmail.com>
2021-03-02 11:13:24 -0700

    updated for last test, testing log in route for app

--- backend/tests/routes/logInTest.test.js
+++ backend/tests/routes/logInTest.test.js
@@ -14,35 +14,35 @@ afterAll(() => {
   mongoose.connection.close();
 });
 
-describe("tests for the log in route", () => {
+describe("Testing POST API endpoints for /accounts/login endpoint", () => {
   test("test log in succeeded username", async function test_log_in(done) {
-    const res = await request(app)
-      .post('/accounts/login')
-      .send({
-        username_or_email: process.env.user,
-        password: process.env.pw 
-      })
-      .expect(201)
-      .expect('Content-Type', /json/)
-      
-      expect(res.body).toHaveProperty('accessToken');
-      expect(res.body).toHaveProperty('refreshToken'); 
-      done(); 
+      const res = await request(app)
+        .post('/accounts/login')
+        .send({
+          username_or_email: process.env.user,
+          password: process.env.pw 
+        })
+        .expect(201)
+        .expect('Content-Type', /json/)
+        
+        expect(res.body).toHaveProperty('accessToken');
+        expect(res.body).toHaveProperty('refreshToken'); 
+        done(); 
   })
 
    test("test log in succeeded email", async function test_log_in(done) {
-    const res = await request(app) 
-      .post('/accounts/login')
-      .send({
-        username_or_email: process.env.user_email,
-        password: process.env.pw 
-      })
-      .expect(201)
-      .expect('Content-Type', /json/)
-      
-      expect(res.body).toHaveProperty('accessToken');
-      expect(res.body).toHaveProperty('refreshToken'); 
-      done();
+      const res = await request(app) 
+        .post('/accounts/login')
+        .send({
+          username_or_email: process.env.user_email,
+          password: process.env.pw 
+        })
+        .expect(201)
+        .expect('Content-Type', /json/)
+        
+        expect(res.body).toHaveProperty('accessToken');
+        expect(res.body).toHaveProperty('refreshToken'); 
+        done();
   })
 
     test("test log in failed -- username/email", async function test_log_in(done) {
@@ -58,5 +58,19 @@ describe("tests for the log in route", () => {
         expect(res.body['message']).toEqual("Username or email is invalid"); 
         done();
   })
+
+    test("test log in failed -- password", async function test_log_in(done) {
+      const res = await request(app) 
+        .post('/accounts/login')
+        .send({
+          username_or_email: process.env.user,
+          password: '123'
+        })
+        .expect(401)
+        .expect('Content-Type', /json/)
+        
+        expect(res.body['message']).toEqual("Password is incorrect"); 
+        done();
+    })
 
 });
