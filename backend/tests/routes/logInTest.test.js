ed340edaf2437bc920aeba2eda0c149ccf2a1e48
13jk59 <jesse.khaira10@gmail.com>
2021-03-02 11:06:31 -0700

    updated tests for logging in using email succesfully

--- backend/tests/routes/logInTest.test.js
+++ backend/tests/routes/logInTest.test.js
@@ -30,52 +30,19 @@ describe("tests for the log in route", () => {
       done(); 
   })
 
-  // test("test log in succeeded email", async function test_log_in(done) {
-  //   const res = await agent
-  //     .post('/accounts/login')
-  //     .send({
-  //       username_or_email: 'practice2@gmail.com',
-  //       password: '123_practice'
-  //     });
+   test("test log in succeeded email", async function test_log_in(done) {
+    const res = await request(app) 
+      .post('/accounts/login')
+      .send({
+        username_or_email: process.env.user_email,
+        password: process.env.pw 
+      })
+      .expect(201)
+      .expect('Content-Type', /json/)
       
-  //     expect(res.status).toEqual(201);
-  //     expect(res.headers['content-type']).toEqual('application/json; charset=utf-8');
-  //     expect(res.body).toHaveProperty('accessToken');
-  //     expect(res.body).toHaveProperty('refreshToken'); 
-  //     done();
-
-  // })
-
-  // test("test log in failed -- username/email", async function test_log_in(done) {
-  //   const res = await agent
-  //     .post('/accounts/login')
-  //     .send({
-  //       username_or_email: '1231293_test',
-  //       password: '123_practice'
-  //     });
-      
-  //     expect(res.status).toEqual(401);
-  //     expect(res.headers['content-type']).toEqual('application/json; charset=utf-8');
-  //     expect(res.body).toHaveProperty('message');
-  //     expect(res.body['message']).toEqual("Username or email is invalid"); 
-  //     done();
-  // })
-
-
-  // test("test log in failed -- password", async function test_log_in(done) {
-  //   const res = await agent
-  //     .post('/accounts/login')
-  //     .send({
-  //       username_or_email: 'test_3',
-  //       password: '1235_practice'
-  //     })
-      
-  //     expect(res.status).toEqual(401);
-  //     expect(res.headers['content-type']).toEqual('application/json; charset=utf-8');
-  //     expect(res.body).toHaveProperty('message');
-  //     expect(res.body['message']).toEqual("Password is incorrect"); 
-  //     (await db).disconnect();
-  //     done();
-  // })
+      expect(res.body).toHaveProperty('accessToken');
+      expect(res.body).toHaveProperty('refreshToken'); 
+      done();
+  })
 
 });
