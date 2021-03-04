7ce97bdca7855ace1606b6fdc366739b26c84221
13jk59 <jesse.khaira10@gmail.com>
2021-03-04 14:22:39 -0700

    added test for testing if new users could be registered appropriately

--- backend/tests/routes/register.test.js
+++ backend/tests/routes/register.test.js
@@ -4,6 +4,7 @@ const app = require('../../app');
 require('dotenv').config({path: path.resolve('.env')}); 
 const mongoose = require('mongoose');
 const mongoDB = process.env.MONGO_URL;
+const User = require("../../models/users").userModel;
 
 const db = mongoose.connect(mongoDB, {
   useNewUrlParser: true,
@@ -16,15 +17,15 @@ afterAll(() => {
 
 test('test register failure -- username already taken', async (done) => {
   let results = await request(app)
-                      .post('/accounts/register')
-                      .send({
-                        'email': 'testing',
-                        'full_name': 'testing',
-                        'username': `${process.env.user}`,
-                        'pw_inp': `${process.env.pw}`,
-                        'date_of_birth': Date.now() 
-                      })
-                      .expect(400); 
+  .post('/accounts/register')
+  .send({
+    'email': 'testing',
+    'full_name': 'testing',
+    'username': process.env.user,
+    'pw_inp': process.env.pw,
+    'date_of_birth': Date.now()
+  })
+  .expect(400); 
 
   expect('This username is already registered to an user').toEqual(results.body.message);
   done(); 
@@ -32,17 +33,37 @@ test('test register failure -- username already taken', async (done) => {
 
 test('test register failure -- email already taken', async (done) => {
   let results = await request(app)
-                      .post('/accounts/register')
-                      .send({
-                        'email': process.env.user_email,
-                        'full_name': 'testing',
-                        'username': 'testing',
-                        'pw_inp': process.env.pw,
-                        'date_of_birth': Date.now()
-                      })
-                      .expect(400); 
+  .post('/accounts/register')
+  .send({
+    'email': process.env.user_email,
+    'full_name': 'testing',
+    'username': 'testing',
+    'pw_inp': process.env.pw,
+    'date_of_birth': Date.now()
+  })
+  .expect(400); 
 
   expect('This email is already registered to an user').toEqual(results.body.message);
   done(); 
 }); 
+
+test('test register success', async (done) => {
+  let results = await request(app)
+  .post('/accounts/register')
+  .send({
+    'email': 'testing@gmail.com',
+    'full_name': 'testing',
+    'username': 'testing123',
+    'pw_inp': process.env.pw,
+    'date_of_birth': Date.now()
+  })
+  .expect(201); 
+
+  expect(results.body).toHaveProperty('accessToken');
+  expect(results.body).toHaveProperty('refreshToken');
+
+  await User.deleteOne({ username: "testing123" });
+
+  done(); 
+})
 
