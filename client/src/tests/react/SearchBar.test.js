afd450b41478f9b16d5bcaf40e482d5bba4c4a7d
13jk59 <jesse.khaira10@gmail.com>
2021-03-21 12:46:55 -0600

    added test

--- client/src/tests/react/SearchBar.test.js
+++ client/src/tests/react/SearchBar.test.js
@@ -1,11 +1,10 @@
+import { act } from "react-dom/test-utils";
+import {SearchBar} from '../../components/LoggedInViews/NavBar/SearchBar';
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-
-import {SearchBar} from '../../components/LoggedInViews/NavBar/SearchBar';
-
 
 let container = null;
+let search_bar_element = null;
 beforeEach(() => {
     // setup a DOM element as a render target
     container = document.createElement("div");
@@ -13,22 +12,23 @@ beforeEach(() => {
     act(() => {
         render(<SearchBar />, container);
     });
+    search_bar_element = document.getElementById('search_bar');
 });
-  
+
 afterEach(() => {
     // cleanup on exiting
     unmountComponentAtNode(container);
     container.remove();
     container = null;
-});
+})
 
 describe('group of tests testing search bar component', () => {
 
-    test('test on click event handler for search bar -- changing elements views appropriately', async () => {
-        const search_bar_element = document.getElementById('search_bar');
+    test('test on click event handler for search bar -- when input tag value == 0 (ie nothing typed in)', async () => {
         const inp_tag = search_bar_element.querySelector('#search_input'); 
         const delete_inp_text_icon = search_bar_element.querySelector('#delete_inp_text_icon');
         const inp_display_text = search_bar_element.querySelector('#inp_display_text');
+
         expect(inp_tag.style.display).toEqual('');
         expect(delete_inp_text_icon.style.display).toEqual('');
         expect(inp_display_text.style.display).toEqual('');
@@ -40,6 +40,29 @@ describe('group of tests testing search bar component', () => {
         expect(inp_tag.style.display).toEqual('block');
         expect(delete_inp_text_icon.style.display).toEqual('block');
         expect(inp_display_text.style.display).toEqual('none');
+
+        expect(inp_tag.contains(document.activeElement)).toEqual(true); 
     });
+
+    test('test on click event handler for search bar -- when input tag value length > 0 ', async () => {
+        const inp_tag = search_bar_element.querySelector('#search_input'); 
+        const search_dropdown_container = search_bar_element.querySelector('#search_dropdown_container');
+        const search_triangle = search_bar_element.querySelector('.search_triangle');
+
+        inp_tag.value = 'testing'; 
+
+        expect(search_dropdown_container.style.display).toEqual('');
+        expect(search_triangle.style.display).toEqual('');
+
+        act(() => {
+            search_bar_element.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+        });
+
+        expect(search_dropdown_container.style.display).toEqual('block');
+        expect(search_triangle.style.display).toEqual('block'); 
+    });
+
+
+
 
 });
