cf57fe218c8442bd57c2c961b9ecdc4f81e74ea3
13jk59 <jesse.khaira10@gmail.com>
2021-04-07 16:34:57 -0600

    got rid of files

--- client/src/tests/react/user-profile-tests/follow-box.test.js
+++
@@ -1,9 +0,0 @@
-import {screen, wait, waitFor} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import {setup_parent_component} from '../test-setup/logged-in-component-setup';
-
-setup_parent_component();
-
-describe('setting up follow box for tests', () => {
-    
-})
