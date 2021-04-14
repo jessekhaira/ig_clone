12b8fee92d48192e50ab5c157ac759aef4aa5b00
13jk59 <jesse.khaira10@gmail.com>
2021-04-14 11:43:41 -0600

    continued migration to typescript, photos mongoose model is now in typescript

---
+++ backend/src/models/photos.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document, Schema, Types } from 'mongoose';
+
+/**
+ * Schema for photos stored in the MongoDB database
+ * @constructor photos
+ */
+const photosSchema: Schema<IPhotos> = new Schema(
+    {
+        data_photo: { type: Buffer, default: 0, required: true },
+        photo_posted_by: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        likes: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'comments',
+            },
+        ],
+    },
+    { timestamps: { createdAt: 'created_at' } },
+);
+
+interface IPhotos extends Document {
+    data_photo: Buffer;
+    photo_posted_by: Types.ObjectId;
+    likes: Array<Types.ObjectId>;
+    comments: Array<Types.ObjectId>;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const photos = mongoose.model('photos', photosSchema);
+exports.photosModel = photos;
