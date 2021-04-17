6445254fcf139dc5f06c90cda28dcd8a3f4af280
13jk59 <jesse.khaira10@gmail.com>
2021-04-17 11:46:08 -0600

    continued migration to typescript, changed login.js to login.ts and used typed everything

---
+++ backend/src/routes/login.ts
@@ -0,0 +1,91 @@
+import express, { Request, Response } from 'express';
+import validator from 'express-validator';
+import dotenv from 'dotenv';
+import path from 'path';
+import { create_access_refresh_tokens } from '../utility/utilityFunctions';
+import { User, IUser } from '../models/users';
+
+dotenv.config({ path: path.resolve('.env') });
+
+interface LoginRequestBody {
+    username_or_email: string;
+    password: string;
+}
+
+/**
+ * Express router to mount login related functions.
+ * @type {object}
+ * @const
+ */
+const router = express.Router();
+
+/**
+ * This API endpoint mounted on the login router responds to GET request to the '/login' route. It has the responsibility of sanitizing
+ * the input arguments recieved, and then validating if they belong to a user in the database. If they do, the client will be notified
+ * that verification was succesful and to redirect to the appropriate view.
+ *
+ * @name post/login
+ * @function
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware
+ */
+router.post('/', [
+    validator.body('username_or_email').escape(),
+    validator.body('password').escape(),
+
+    async (
+        req: Request<
+            Record<string, never>,
+            Record<string, never>,
+            LoginRequestBody
+        >,
+        res: Response,
+    ) => {
+        const { username_or_email } = req.body;
+        const { password } = req.body;
+        // find user
+        try {
+            const user: IUser = await User.findOne({
+                $or: [
+                    { email: username_or_email },
+                    { username: username_or_email },
+                ],
+            });
+
+            if (!user) {
+                return res
+                    .status(401)
+                    .json({ message: 'Username or email is invalid' });
+            }
+            const pwVerification = await user.verifyPassword(password);
+            if (pwVerification === false) {
+                return res
+                    .status(401)
+                    .json({ message: 'Password is incorrect' });
+            }
+
+            // if user is found, password is verified, then we make a jwt
+            // access token and request token and return both of them to the client
+            const [accessToken, refreshToken] = create_access_refresh_tokens(
+                user.username,
+            );
+            // update refresh token in the db for the user to be this new refresh token
+            user.refreshToken = refreshToken;
+            await user.save();
+            return res.status(201).json({ accessToken, refreshToken });
+        } catch (err) {
+            return res
+                .status(500)
+                .json({ Error: 'Error processing login request' });
+        }
+    },
+]);
+
+router.get('/', (req, res) => {
+    return res.sendFile(
+        path.join(__dirname, '../../../client/build/index.html'),
+    );
+});
+
+exports.login_router = router;
+export default router;
--- backend/src/utility/utilityFunctions.js
+++ backend/src/utility/utilityFunctions.js
@@ -1,10 +1,10 @@
-const path = require('path'); 
-require('dotenv').config({path: path.resolve(".env")}); 
-const jwt = require('jsonwebtoken'); 
+const path = require('path');
+require('dotenv').config({ path: path.resolve('.env') });
+const jwt = require('jsonwebtoken');
 
 /**
  * Object with a mapping between string numbers and the month they represent. Useful when processing
- * date information. 
+ * date information.
  */
 const monthMapping = {
     1: 'JANUARY',
@@ -18,83 +18,92 @@ const monthMapping = {
     9: 'SEPTEMBER',
     10: 'OCTOBER',
     11: 'NOVEMBER',
-    12: 'DECEMBER'
+    12: 'DECEMBER',
 };
 
-
 /**
- * This function has the responsibility of converting an array of objects, each representing a Mongoose document, 
- * to an array of Javascript objects with the purpose of converting binart vuffers representing images to base64 
+ * This function has the responsibility of converting an array of objects, each representing a Mongoose document,
+ * to an array of Javascript objects with the purpose of converting binart vuffers representing images to base64
  * encoded strings.
- * 
+ *
  */
 function convertArrayPicBuffers2Base64(documents, property) {
-    let return_arr = []; 
+    const return_arr = [];
     for (let doc of documents) {
         doc = convertBuffer2Base64(doc, property);
-        return_arr.push(doc); 
+        return_arr.push(doc);
     }
-    return return_arr; 
+    return return_arr;
 }
 
 /**
- * This function accepts two arguments: a Mongoose document and a string. The string represents the property in the 
+ * This function accepts two arguments: a Mongoose document and a string. The string represents the property in the
  * mongoose document that holds a buffer representing an image. The Mongoose document is converted to a Javascript
- * object, and the binary buffer is converted to a base64 encoded string. 
+ * object, and the binary buffer is converted to a base64 encoded string.
  */
 function convertBuffer2Base64(doc, property) {
-    doc = doc.toObject(); 
-    doc[property] = doc[property].toString('base64'); 
-    return doc; 
+    doc = doc.toObject();
+    doc[property] = doc[property].toString('base64');
+    return doc;
 }
 
 /**
- * This function accepts two numbers representing dates, and returns the differential in time between the dates. 
+ * This function accepts two numbers representing dates, and returns the differential in time between the dates.
  * According to the differential in time between the dates, the appropriate time is returned.
- * 
+ *
  * @param {number} date1 Number representing a date
  * @param {number} date2 Number representing a date
  */
 function getDateDifferential(date1, date2) {
     const diffMilliseconds = Math.abs(date2 - date1);
-    const diffMinutes = Math.ceil(diffMilliseconds/(1000*60));
-    const diffHours = Math.ceil(diffMinutes/60);
-    const diffDays = Math.ceil(diffHours / (24)); 
+    const diffMinutes = Math.ceil(diffMilliseconds / (1000 * 60));
+    const diffHours = Math.ceil(diffMinutes / 60);
+    const diffDays = Math.ceil(diffHours / 24);
 
     // based on the time differential, return the appropriate period of time (ie: minutes, hours, etc)
     if (diffMinutes < 60) {
-        let descr_text = diffMinutes === 1? `MINUTE AGO`: `MINUTES AGO`;
+        const descr_text = diffMinutes === 1 ? `MINUTE AGO` : `MINUTES AGO`;
         return `${diffMinutes} ${descr_text}`;
     }
-    else if (diffHours < 24) {
-        let descr_text = diffHours === 1 ? 'HOUR AGO': 'HOURS AGO';
-        return `${diffHours} ${descr_text}`
+    if (diffHours < 24) {
+        const descr_text = diffHours === 1 ? 'HOUR AGO' : 'HOURS AGO';
+        return `${diffHours} ${descr_text}`;
     }
-    else if (diffDays < 8) {
-        let descr_text = diffDays === 1 ? 'DAY AGO': 'DAYS AGO';
+    if (diffDays < 8) {
+        const descr_text = diffDays === 1 ? 'DAY AGO' : 'DAYS AGO';
         return `${diffDays} ${descr_text}`;
     }
-    else {
-        const month = monthMapping[date1.getUTCMonth() + 1]; 
-        const day = date1.getUTCDate();
-        const year = date1.getUTCFullYear();
-        return `${month} ${day}, ${year}`;
-    }
-}
 
+    const month = monthMapping[date1.getUTCMonth() + 1];
+    const day = date1.getUTCDate();
+    const year = date1.getUTCFullYear();
+    return `${month} ${day}, ${year}`;
+}
 
 /**
  * This purpose of this function is to produce JSON web tokens for a user that has been authorized.
- * @param {String} username String representing the username of a given user 
- * @returns {String[]} Array of 2 strings, with each string representing a JSON web token 
+ * @param {String} username String representing the username of a given user
+ * @returns {String[]} Array of 2 strings, with each string representing a JSON web token
  */
 function create_access_refresh_tokens(username) {
-    const accessToken = jwt.sign({username: username}, process.env.ACESS_TOKEN_SECRET, {expiresIn: '20m'});
-    const refreshToken = jwt.sign({username: username}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '3d'});
+    const accessToken = jwt.sign({ username }, process.env.ACESS_TOKEN_SECRET, {
+        expiresIn: '20m',
+    });
+    const refreshToken = jwt.sign(
+        { username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '3d' },
+    );
     return [accessToken, refreshToken];
 }
 
-exports.convertArrayPicBuffers2Base64 = convertArrayPicBuffers2Base64; 
-exports.convertBuffer2Base64 = convertBuffer2Base64; 
-exports.create_access_refresh_tokens = create_access_refresh_tokens; 
-exports.getDateDifferential = getDateDifferential; 
+export {
+    create_access_refresh_tokens,
+    convertArrayPicBuffers2Base64,
+    convertBuffer2Base64,
+    getDateDifferential,
+};
+exports.convertArrayPicBuffers2Base64 = convertArrayPicBuffers2Base64;
+exports.convertBuffer2Base64 = convertBuffer2Base64;
+exports.create_access_refresh_tokens = create_access_refresh_tokens;
+exports.getDateDifferential = getDateDifferential;
