7f1affb03ac17bed4cb47ddd9f3c56945fd69079
13jk59 <jesse.khaira10@gmail.com>
2021-05-04 11:56:37 -0600

    cleaned up explore.js and migrated it to typescript

---
+++ backend/src/routes/explore.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.resolve('.env') });
+
+/**
+ * Express router to mount explore related functions.
+ * @type {object}
+ * @const
+ */
+const router = express.Router();
+
+// deals with case when page is refreshed and the user is logged in -- returns the appropriate view
+// for explore page
+router.get('/', (req: Request, res: Response) => {
+    return res.sendFile(
+        path.join(__dirname, '../../../client/build/index.html'),
+    );
+});
+
+exports.explore_router = router;
+export default router;
