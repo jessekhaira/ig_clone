a3f7a52340d48e9698eee572d24e58ae32e26946
13jk59 <jesse.khaira10@gmail.com>
2021-05-04 11:54:06 -0600

    updated refreshTokenRoute.test.js

--- backend/src/tests/routes/refreshTokenRoute.test.js
+++ backend/src/tests/routes/refreshTokenRoute.test.js
@@ -1,26 +1,26 @@
-const request = require('supertest'); 
+const request = require('supertest');
 const app = require('../../app');
-const setupLocalDatabase = require('../database_setup').setupLocalDatabase; 
+const { setupLocalDatabase } = require('../database_setup');
+
 setupLocalDatabase(`refreshTokenRouteTest`);
 
-
 test('Test if refresh token GET route functioning properly', async (done) => {
-    let login_tokens_res = await request(app)
-      .post('/accounts/login')
-      .send({
-        username_or_email: `testUser8`,
-        password: `123456` 
-      })
-      .expect(201);
+    const login_tokens_res = await request(app)
+        .post('/accounts/login')
+        .send({
+            username_or_email: `testUser8`,
+            password: `123456`,
+        })
+        .expect(201);
 
-    let refresh_token= login_tokens_res.body.refreshToken; 
-    
-    let new_access_token_res = await request(app)
-      .get('/accounts/refreshToken')
-      .set('Authorization', `${refresh_token}`)
-      .expect(201)
-      .expect('Content-Type', /json/)
-    
-    expect(new_access_token_res.body).toHaveProperty('new_access_token');                                      
-    done(); 
-})
+    const refresh_token = login_tokens_res.body.refreshToken;
+
+    const new_access_token_res = await request(app)
+        .get('/accounts/refreshToken')
+        .set('Authorization', `${refresh_token}`)
+        .expect(201)
+        .expect('Content-Type', /json/);
+
+    expect(new_access_token_res.body).toHaveProperty('new_access_token');
+    done();
+});
