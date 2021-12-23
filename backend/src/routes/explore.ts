d4fe5c6f2ee0c448ce21bf044c9dced123455f45
13jk59 <jesse.khaira10@gmail.com>
2021-12-22 21:28:33 -0700

    linting

--- backend/src/routes/explore.ts
+++ backend/src/routes/explore.ts
@@ -11,8 +11,8 @@ dotenv.config({ path: path.resolve('.env') });
  */
 const router = express.Router();
 
-// deals with case when page is refreshed and the user is logged in -- returns the appropriate view
-// for explore page
+/* Deals with case when page is refreshed and the user is logged
+in -- returns the appropriate view for explore page */
 router.get('/', (req: Request, res: Response) => {
     return res.sendFile(
         path.join(__dirname, '../../../client/build/index.html'),
